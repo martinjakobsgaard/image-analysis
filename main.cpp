97496c10c365525b1e2fd2e023efc969122a0520
mathiaslyngbye <malyn16@student.sdu.dk>
2020-09-21 15:55:37 +0200

    Take image input, ready for merge

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
-int main()
+int main( int argc, char** argv )
 {
+    std::String imageName;
+    if( argc > 1)
+        imageName = argv[1];
+
+    cv::Mat image;
+    image = cv::imread( samples::findFile( imageName ), cv::IMREAD_COLOR );
+    if( image.empty() )
+    {
+        std:.cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+
     cout << "Hello World!" << endl;
     return 0;
 }
