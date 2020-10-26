c7a26b14688b280c3d84938ee897f22fadc5c45a
mathiaslyngbye <malyn16@student.sdu.dk>
2020-10-26 11:08:40 +0100

    Clean project

---
+++ segmentation.cpp
@@ -0,0 +1,13 @@
+// Standard includes
+#include <iostream>
+
+// Include OpenCV API
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+
+int main( int argc, char** argv )
+{
+    std::cout << "Hello" << std::endl;
+
+    return 0;
+}
