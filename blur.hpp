dd3d0fdf7c71999fcf7ad0d643caefe07d9cebd7
mathiaslyngbye <malyn16@student.sdu.dk>
2020-10-30 14:16:10 +0100

    Take directory as input, and move test to class

--- blur.hpp
+++ blur.hpp
@@ -1,153 +1,158 @@
 #ifndef BLUR_HPP
 #define BLUR_HPP
 
-#include <iostream>
-
-// Include OpenCV API
+// OpenCV includes
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 
+// Standard includes
 #include <iostream>
 #include <string>
+#include <regex>
 
-#include "utility.hpp"
-
-//const std::string path = ":/images/";
-//const std::string dataset = "exposure_test_light";
-
-int getBlurValueLaplacian(cv::Mat input)
+class Blur
 {
-    //https://stackoverflow.com/questions/24080123/opencv-with-laplacian-formula-to-detect-image-is-blur-or-not-in-ios
-    cv::Mat matImageGrey;
-    cv::cvtColor(input, matImageGrey, CV_BGR2GRAY);
-
-    cv::Mat laplacianImage;
-    //cv::Laplacian(matImageGrey, laplacianImage, CV_8U);
-
-    //cv::Mat laplacianImage8bit;
-    //laplacianImage.convertTo(laplacianImage8bit, CV_8UC1);
-
-    cv::Laplacian(matImageGrey, laplacianImage, CV_64F);
-    cv::Scalar mean, stddev; // 0:1st channel, 1:2nd channel and 2:3rd channel
-    cv::meanStdDev(laplacianImage, mean, stddev, cv::Mat());
-    double variance = stddev.val[0] * stddev.val[0];
-
-    return variance;
-}
-
-int getBlurValueVert(cv::Mat input)
-{
-    int blurVal = 0;
-
-    cv::Mat matImageGrey;
-    cv::cvtColor(input, matImageGrey, CV_BGR2GRAY);
-
-    for(int i = 1; i < matImageGrey.size().height-1; i++)
+private:
+    std::vector<std::string> image_paths;
+public:
+    Blur(std::vector<std::string> _image_paths)
     {
-        for(int j = 0; j < matImageGrey.size().width; j++)
-        {
-            int pixelAbove = matImageGrey.at<uchar>(i-1,j);
-            int pixel = matImageGrey.at<uchar>(i,j);
-            int pixelBelow = matImageGrey.at<uchar>(i+1,j);
-
-            blurVal += (pixelAbove*1) + (pixel*-2) + (pixelBelow*1);
-        }
+        image_paths = _image_paths;
     }
 
-    return blurVal;
-}
-
-int getBlurValueHor(cv::Mat input)
-{
-    int blurVal = 0;
-
-    cv::Mat matImageGrey;
-    cv::cvtColor(input, matImageGrey, CV_BGR2GRAY);
-
-    for(int j = 1; j < matImageGrey.size().width-1; j++)
+    int getBlurValueLaplacian(cv::Mat input)
     {
-        for(int i = 0; i < matImageGrey.size().height; i++)
-        {
-            int pixelLeft = matImageGrey.at<uchar>(i,j-1);
-            int pixel = matImageGrey.at<uchar>(i,j);
-            int pixelRight = matImageGrey.at<uchar>(i,j+1);
+        //https://stackoverflow.com/questions/24080123/opencv-with-laplacian-formula-to-detect-image-is-blur-or-not-in-ios
+        cv::Mat matImageGrey;
+        cv::cvtColor(input, matImageGrey, CV_BGR2GRAY);
 
-            blurVal += (pixelLeft*1) + (pixel*-2) + (pixelRight*1);
-        }
+        cv::Mat laplacianImage;
+        //cv::Laplacian(matImageGrey, laplacianImage, CV_8U);
+
+        //cv::Mat laplacianImage8bit;
+        //laplacianImage.convertTo(laplacianImage8bit, CV_8UC1);
+
+        cv::Laplacian(matImageGrey, laplacianImage, CV_64F);
+        cv::Scalar mean, stddev; // 0:1st channel, 1:2nd channel and 2:3rd channel
+        cv::meanStdDev(laplacianImage, mean, stddev, cv::Mat());
+        double variance = stddev.val[0] * stddev.val[0];
+
+        return variance;
     }
 
-    return blurVal;
-}
-
-cv::Mat convertHistogramEqualizer(cv::Mat input)
-{
-    cv::Mat hist_equalized_image;
-    cv::cvtColor(input, hist_equalized_image, cv::COLOR_BGR2YCrCb);
-
-    //Split the image into 3 channels; Y, Cr and Cb channels respectively and store it in a std::vector
-    std::vector<cv::Mat> vec_channels;
-    cv::split(hist_equalized_image, vec_channels);
-
-    //Equalize the histogram of only the Y channel
-    cv::equalizeHist(vec_channels[0], vec_channels[0]);
-
-    //Merge 3 channels in the vector to form the color image in YCrCB color space.
-    cv::merge(vec_channels, hist_equalized_image);
-
-    //Convert the histogram equalized image from YCrCb to BGR color space again
-    cv::cvtColor(hist_equalized_image, hist_equalized_image, cv::COLOR_YCrCb2BGR);
-
-    return hist_equalized_image;
-}
-
-void exportBlurData(int image_count, char** image_name, std::string file_name = "data.csv")
-{
-    // TESTING
-    /*
-    std::vector<std::string> filenames;
-    get_filenames(filenames, "./");
-    for(int i = 0; i<filenames.size(); i++)
-        std::cout << filenames[i] << std::endl;
-    */
-    std::cout << "Exporting blur data to \"" << file_name << "\"..." << std::endl;
-
-    // file pointer
-    std::fstream fout;
-
-    // opens an existing csv file or creates a new file.
-    fout.open(file_name, std::ios::out);
-
-    std::array<std::string,5> delimiter = {"_i", "_s", "_z", "_e", ".jpg"};
-
-    fout << "index" << ',' << "sequence" << ',' << "robotz" << ',' << "exposure" << ',' << "blur" << '\n';
-
-    for(int i = 1; i < image_count; i++)
+    int getBlurValueVert(cv::Mat input)
     {
-        std::string imageName = image_name[i];
+        int blurVal = 0;
 
-        cv::Mat image;
-        image = cv::imread(imageName, cv::IMREAD_COLOR );
+        cv::Mat matImageGrey;
+        cv::cvtColor(input, matImageGrey, CV_BGR2GRAY);
 
-        if( image.empty() )
+        for(int i = 1; i < matImageGrey.size().height-1; i++)
         {
-            std::cout <<  "Could not open or find the image" << std::endl ;
+            for(int j = 0; j < matImageGrey.size().width; j++)
+            {
+                int pixelAbove = matImageGrey.at<uchar>(i-1,j);
+                int pixel = matImageGrey.at<uchar>(i,j);
+                int pixelBelow = matImageGrey.at<uchar>(i+1,j);
+
+                blurVal += (pixelAbove*1) + (pixel*-2) + (pixelBelow*1);
+            }
+        }
+
+        return blurVal;
+    }
+
+    int getBlurValueHor(cv::Mat input)
+    {
+        int blurVal = 0;
+
+        cv::Mat matImageGrey;
+        cv::cvtColor(input, matImageGrey, CV_BGR2GRAY);
+
+        for(int j = 1; j < matImageGrey.size().width-1; j++)
+        {
+            for(int i = 0; i < matImageGrey.size().height; i++)
+            {
+                int pixelLeft = matImageGrey.at<uchar>(i,j-1);
+                int pixel = matImageGrey.at<uchar>(i,j);
+                int pixelRight = matImageGrey.at<uchar>(i,j+1);
+
+                blurVal += (pixelLeft*1) + (pixel*-2) + (pixelRight*1);
+            }
+        }
+
+        return blurVal;
+    }
+
+    cv::Mat convertHistogramEqualizer(cv::Mat input)
+    {
+        cv::Mat hist_equalized_image;
+        cv::cvtColor(input, hist_equalized_image, cv::COLOR_BGR2YCrCb);
+
+        //Split the image into 3 channels; Y, Cr and Cb channels respectively and store it in a std::vector
+        std::vector<cv::Mat> vec_channels;
+        cv::split(hist_equalized_image, vec_channels);
+
+        //Equalize the histogram of only the Y channel
+        cv::equalizeHist(vec_channels[0], vec_channels[0]);
+
+        //Merge 3 channels in the vector to form the color image in YCrCB color space.
+        cv::merge(vec_channels, hist_equalized_image);
+
+        //Convert the histogram equalized image from YCrCb to BGR color space again
+        cv::cvtColor(hist_equalized_image, hist_equalized_image, cv::COLOR_YCrCb2BGR);
+
+        return hist_equalized_image;
+    }
+
+    void exportBlurData(std::string file_name = "data.csv")
+    {
+        if(image_paths.empty())
+        {
+            std::cerr << "No images!" << std::endl;
             return;
         }
 
-        for(int j = 0; j < delimiter.size()-1; j++)
+        std::cout << "Exporting data to \"" << file_name << "\"..." << std::endl;
+
+        // file pointer
+        std::fstream fout;
+
+        // opens an existing csv file or creates a new file.
+        fout.open(file_name, std::ios::out);
+
+        std::array<std::string,5> delimiter = {"_i", "_s", "_z", "_e", ".jpg"};
+
+        fout << "index" << ',' << "sequence" << ',' << "robotz" << ',' << "exposure" << ',' << "blur" << '\n';
+
+        for(int i = 0; i < image_paths.size(); i++)
         {
-            std::regex base_regex(delimiter[j] + "(.*)" + delimiter[j+1]);
-            std::smatch base_match;
-            std::regex_search(imageName, base_match, base_regex);
-            fout << base_match[1].str() << ',';
+            std::string imageName = image_paths[i];
+
+            cv::Mat image;
+            image = cv::imread(imageName, cv::IMREAD_COLOR );
+
+            if( image.empty() )
+            {
+                std::cout <<  "Could not open or find the image" << std::endl ;
+                return;
+            }
+
+            for(int j = 0; j < delimiter.size()-1; j++)
+            {
+                std::regex base_regex(delimiter[j] + "(.*)" + delimiter[j+1]);
+                std::smatch base_match;
+                std::regex_search(imageName, base_match, base_regex);
+                fout << base_match[1].str() << ',';
+            }
+            fout << getBlurValueLaplacian(convertHistogramEqualizer(image)) << '\n';
+
+            // Progress
+            std::cout << "\rProgress: " << (i+1) << "/" << image_paths.size() << std::flush;
         }
-        fout << getBlurValueLaplacian(convertHistogramEqualizer(image)) << '\n';
 
-        // Progress
-        std::cout << "\rProgress: " << (i+1) << "/" << image_count << std::flush;
+        std::cout << std::endl;
+        std::cout << "Done!" << std::endl;
     }
-    std::cout << std::endl;
-    std::cout << "Done!" << std::endl;
-}
-
+};
 #endif // BLUR_HPP
--- main.cpp
+++ main.cpp
@@ -1,25 +1,43 @@
+// Standard includes
 #include <iostream>
-
-// Include OpenCV API
-#include <opencv2/opencv.hpp>
-#include <opencv2/core.hpp>
-
-// Include standard
-#include <iostream>
+#include <vector>
 #include <string>
-#include <regex>
+#include <experimental/filesystem>
 
+// Test method includes
 #include "blur.hpp"
 #include "segmentation.hpp"
 
 int main( int argc, char** argv )
 {
-    std::cout << "Started image analysis!" << std::endl;
-
-    if( argc <= 1)
-        return -1;
-
-    exportBlurData(argc, argv, "test_data.csv");
+    // Fetch input file path
+    std::string path = "./";
+    if( argc == 2)
+        path = argv[1];
+    std::cout << "Dataset path: \"" << path << "\"" << std::endl;
+    
+    // Generate vector of paths to supported files
+    std::vector<std::string> image_paths;
+    std::vector<std::string> supported_file_extensions = {"jpg", "png"};
+    for (const auto & entry : std::experimental::filesystem::directory_iterator(path))
+    {
+        std::string path_string = entry.path();
+        std::string file_extension = path_string.substr(path_string.find_last_of(".") + 1); 
+        
+        for(const auto & supported_file_extension : supported_file_extensions)
+        {
+            if(file_extension == supported_file_extension)
+            {
+                image_paths.push_back(path_string);
+                break;
+            }
+        }
+    }
+    std::cout << "Found " << image_paths.size() << " image(s)!" << std::endl;
+    
+    // Do something with files
+    Blur my_test(image_paths);
+    my_test.exportBlurData("test_data.csv");
 
     return 0;
 }
