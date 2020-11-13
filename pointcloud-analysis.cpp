2a1cf4160fe7d46da0d63f9b26ee6bfb8cb4e012
mathiaslyngbye <malyn16@student.sdu.dk>
2020-11-13 10:50:12 +0100

    Add pc analysis

---
+++ pointcloud-analysis.cpp
@@ -0,0 +1,16 @@
+// Standard includes
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main( int argc, char** argv )
+{
+    // Fetch input file path
+    //std::string path = "./";
+    //if( argc < 2)
+    //    return -1; // Error
+
+    std::cout << "Hello darkness my old friend" << std::endl;
+
+    return 0;
+}
